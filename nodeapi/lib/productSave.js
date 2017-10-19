7c857777bf8c78c10a84ca331e69d92722d5636d
edu <vizuete.eduardo@gmail.com>
2017-10-19 14:57:32 -0500

    Update script save products installDB

--- nodeapi/lib/productSave.js
+++ nodeapi/lib/productSave.js
@@ -2,21 +2,24 @@
 
 var productSchema = require('../models/productSchema');
 
-// save product
-var prod = new productSchema({
-    name: 'nameprod1',
-	description:'descprod1',
-    category: 'cat1',
-    seller: 'username1',
-    published_date: '07/09/2017',
-    state: 'selling',
-    price: 10.5
-});
+for (var i=0; i<10; i++) {
+    // save product
+    var prod = new productSchema({
+        name: 'Name ' + i,
+        description:'Description ' + i,
+        category: 'Category ' + i,
+        seller: 'Seller username' + 1,
+        published_date: '07/09/2017',
+        state: 'selling',
+        price: i
+    });
 
-prod.save(function (err, prodSaved) {
-	if (err) {
-		next(err);
-		return;
-	}
-	console.log('Product saved', prodSaved);
-});
+    prod.save(function (err, prodSaved) {
+        if (err) {
+            console.log('Product error: ', prodSaved);
+            next(err);
+            return;
+        }
+        console.log('Product saved', prodSaved);
+    });
+}
